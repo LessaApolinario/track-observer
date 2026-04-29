e94e0fa563b04cfc26cd4eec381a55a179f85bb8
Lessa Apolinario <lessaapolinario@gmail.com>
2026-04-29 11:29:06 -0300

    Reformat code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import { TrackProvider } from "@/ui/contexts/TrackProvider"
-import type { Metadata } from "next"
-import { Manrope } from "next/font/google"
-import "./globals.css"
+import { TrackProvider } from '@/ui/contexts/TrackProvider'
+import type { Metadata } from 'next'
+import { Manrope } from 'next/font/google'
+import './globals.css'
 
 const manrope = Manrope({
-  subsets: ["latin"],
-  variable: "--font-manrope",
-  weight: ["400", "500", "600", "700", "800"],
+  subsets: ['latin'],
+  variable: '--font-manrope',
+  weight: ['400', '500', '600', '700', '800'],
 })
 
 export const metadata: Metadata = {
-  title: "Track Observer",
-  description: "A tool to check the currently playing track on Spotify",
+  title: 'Track Observer',
+  description: 'A tool to check the currently playing track on Spotify',
 }
 
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${manrope.variable} h-full antialiased`}>
-      <body className="min-h-full font-sans text-foreground">
+      <body className="text-foreground min-h-full font-sans">
         <TrackProvider>{children}</TrackProvider>
       </body>
     </html>
